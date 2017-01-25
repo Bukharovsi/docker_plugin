9813d37a211021f466d165ab3cfd9dee51e3efd8
Bukharov <sergey.bukharov@rtlabs.ru>
2017-01-25 18:35:33 +0300

    edit ReportCollection

--- src/Docker/Report/ReportCollection.php
+++ src/Docker/Report/ReportCollection.php
@@ -11,12 +11,20 @@ namespace Bukharovsi\DockerPlugin\Docker\Report;
 
 use Bukharovsi\DockerPlugin\Docker\Configuration\IConfiguration;
 use Bukharovsi\DockerPlugin\Docker\Image\BuiltImage;
+use Bukharovsi\DockerPlugin\Docker\Report\Teamcity\TeamcityBuiltImageVersionReport;
 use Symfony\Component\Console\Output\Output;
 
 class ReportCollection implements IReport
 {
+
+    private $registeredReports = [
+        'console' => SimpleOutputReport::class,
+        'teamcity' => TeamcityBuiltImageVersionReport::class
+    ];
+
     public function __construct(BuiltImage $builtImage, IConfiguration $conf, Output $output)
     {
+
     }
 
     public function make()
